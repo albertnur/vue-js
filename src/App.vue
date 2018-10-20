8cba97e66d58711b50a77546a2880bfa0852c2bc
albertnur <43085647+albertnur@users.noreply.github.com>
2018-10-20 14:27:56 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NavbarStarwars></NavbarStarwars>
+    <FloatButton></FloatButton>
+  </div>
+</template>
+
+<script>
+import FloatButton from "./components/FloatButton";
+import NavbarStarwars from "./components/NavbarStarwars";
+export default {
+  name: "App",
+  components: {
+    NavbarStarwars,
+    FloatButton
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  height: 100vh;
+  background: #ffffff;
+  position: relative;
+}
+</style>
